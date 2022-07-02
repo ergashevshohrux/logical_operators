8538e03d25bb372ea8212bea9c23a91a5345957d
Shohrux <shohruxergashev1204@gmail.com>
2022-07-02 11:13:13 +0500

    logical

--- bin/logical_operators16.dart
+++ bin/logical_operators16.dart
@@ -7,5 +7,9 @@ Given integer a,  check the following statement "The integer is a five-digit num
     Returns:
         bool: answer
 */
-
-void main() {}
+bool func(int a ) {
+    return a > 9999 && a < 100000;
+}
+void main() {
+    print(func(54321));
+}
