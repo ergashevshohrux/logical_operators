9eb77d5da45ff770a8567e19c86da968296de574
Shohrux <shohruxergashev1204@gmail.com>
2022-07-02 10:09:43 +0500

    logical

--- bin/logical_operators01.dart
+++ bin/logical_operators01.dart
@@ -9,5 +9,14 @@ Given three integers a, b, c,  check the following statement "The number b is be
     Returns:
         bool: answer
 */
+bool func(a, b, c) {
+    return 3 > 5 && 5 < 8;
 
-void main() {}
+}
+
+
+
+void main() {
+    print(func(3, 5, 8));
+
+}
