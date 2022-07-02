3772ed4a15a88e72d886beec5ae306e29c3ef45d
Shohrux <shohruxergashev1204@gmail.com>
2022-07-02 12:24:16 +0500

    logical

--- bin/logical_operators20.dart
+++ bin/logical_operators20.dart
@@ -9,5 +9,26 @@ Five number consisting of one and zero is given (the number starts at once).
     Returns:
         bool: answer
 */
+bool func (n){
+    int a = n % 10;
+    n = n~/ 10 ;
+    int b = n % 10;
+    n = n ~/ 10;
+    int c = n % 10;
+    n = n ~/ 10;
+    int d = n % 10;
+    int f = n ~/ 10;
+    int bir = a + b + c + d + f;
+    int nol = 5 - bir;
+    if ( bir > nol && f==1&& a>0 ){
+        return true;
+    } else return false;
+}
 
-void main() {}
+
+
+
+void main () {
+    print(func(10101));
+
+}
