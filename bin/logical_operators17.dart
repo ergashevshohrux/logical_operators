54b1184516aa6b2e5ef28e5da3c9983b3cfc45fd
Shohrux <shohruxergashev1204@gmail.com>
2022-07-02 11:18:50 +0500

    logical

--- bin/logical_operators17.dart
+++ bin/logical_operators17.dart
@@ -7,5 +7,17 @@ Given a five-digit integer a,  check the following statement "All digits of the 
     Returns:
         bool: answer
 */
-
-void main() {}
+bool func(a) {
+    int a = 2;
+    int b = 4;
+    int c = 6;
+    int d = 8;
+    int e = 7;
+    if (a < b && b < c && c < d && d < e) {
+        return true;
+    }
+    return false;
+}
+void main() {
+    print(func(98769));
+}
